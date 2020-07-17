519d245652f6588bca54b7fc9999093a50218ee5
Elfebleu9 <poetic.jm@orange.fr>
2020-07-17 17:28:12 +0200

    Setup PinsController, Ad Psysh Bundle, pluralize twig function

---
+++ .history/src/Controller/PinsController_20200716202347.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\PinRepository;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class PinsController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_home")
+     */
+    public function index(PinRepository $pinRepository)
+    {
+        return $this->render('pins/index.html.twig');
+    }
+}
---
+++ .history/src/Controller/PinsController_20200716202644.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\PinRepository;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class PinsController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_home")
+     */
+    public function index(PinRepository $pinRepository)
+    {
+        $pins=$pinRepository->FindAll();
+        return $this->render('pins/index.html.twig');
+    }
+}
