1a0b3bf6eeaa6e126c285e5ec99f8d0b547e4865
Elfebleu9 <poetic.jm@orange.fr>
2020-07-25 14:54:53 +0200

    ajout champ image dans table Pin

--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -46,6 +46,11 @@ class Pin
      */
     private $updatedAt;
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $imageName;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -111,5 +116,17 @@ class Pin
         
         $this->setUpdatedAt(new \DateTimeImmutable);
 
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
+    }
+
+    public function setImageName(?string $imageName): self
+    {
+        $this->imageName = $imageName;
+
+        return $this;
     }
 }
